f2dd1689b10d4cd05174095bd66ff0a5b8825fb6
Dev <86075203+web-co1@users.noreply.github.com>
2024-02-05 21:43:53 +0500

    Add files via upload

---
+++ updated-soho-lat.js
@@ -0,0 +1,304 @@
+var soho__size_72 = {
+  white: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0fb7b206196c4fe4ee7c0_image%2022%20(1).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c0ff7e99f5e94a014bd404_kitchen%202%20(26).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c0ffbb03d5d750958f922e_image%2022%20(4).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c100104f860782fe457161_kitchen%202%20(27).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c100769b32f12b15eef313_replica%20(1).png",
+    ],
+  },
+  grey: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0fbeb17e8b72dc9febe6a_image%2021%20(1).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c101151ea864f34202308a_kitchen%202%20(28).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c1019904daf8fdc62b6956_image%2022%20(5).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c101c02e74ce3b9d50e621_kitchen%202%20(29).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c1020843dc56d94296ce86_replicate%20(1).png",
+    ],
+  },
+
+  dark: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0fc76afa8fa3df2e3a9e6_image%2021%20(2).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c10245ec8e3abfc796a1e5_kitchen%202%20(30).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c102ce79378d9a117f347f_image%2022%20(6).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c103740838c9d43e5ff5fe_kitchen%202%20(31).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c103e5b888d8bf4143f485_repl%20(1).png",
+    ],
+  },
+
+  green: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0fcc1661b879f3451bc8f_image%2021%20(3).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c10447ec8e3abfc797fc42_kitchen%202%20(32).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c1048d341185f44c79c8e4_image%2022%20(7).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c10510c177319cdb01a482_kitchen%202%20(33).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c10587a4431d4d32093cc3_repli%20(1).png",
+    ],
+  },
+};
+
+var soho__size_60 = {
+  white: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0fb7b206196c4fe4ee7c0_image%2022%20(1).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c0ff7e99f5e94a014bd404_kitchen%202%20(26).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c0ffbb03d5d750958f922e_image%2022%20(4).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c100104f860782fe457161_kitchen%202%20(27).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c100769b32f12b15eef313_replica%20(1).png",
+    ],
+  },
+  grey: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0fbeb17e8b72dc9febe6a_image%2021%20(1).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c101151ea864f34202308a_kitchen%202%20(28).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c1019904daf8fdc62b6956_image%2022%20(5).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c101c02e74ce3b9d50e621_kitchen%202%20(29).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c1020843dc56d94296ce86_replicate%20(1).png",
+    ],
+  },
+
+  dark: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0fc76afa8fa3df2e3a9e6_image%2021%20(2).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c10245ec8e3abfc796a1e5_kitchen%202%20(30).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c102ce79378d9a117f347f_image%2022%20(6).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c103740838c9d43e5ff5fe_kitchen%202%20(31).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c103e5b888d8bf4143f485_repl%20(1).png",
+    ],
+  },
+
+  green: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0fcc1661b879f3451bc8f_image%2021%20(3).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c10447ec8e3abfc797fc42_kitchen%202%20(32).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c1048d341185f44c79c8e4_image%2022%20(7).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c10510c177319cdb01a482_kitchen%202%20(33).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c10587a4431d4d32093cc3_repli%20(1).png",
+    ],
+  },
+};
+
+var soho__size_48 = {
+    white: {
+      mainImg:
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c015bf1b67fbfec935e85d_image%2021.png",
+      sliders: [
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c0162c60bcfa83d9c6beaf_kitchen%202%20(6).png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c0167ead4984d075d59e62_image%2022.png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c016b3a7461fca4f366546_kitchen%202%20(7).png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c016f1e25c9e1161dab175_rel.png",
+      ],
+    },
+    grey: {
+      mainImg:
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c017494e8a00ec77b58761_image%2021%20(1).png",
+      sliders: [
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c0179c6c2c54376b954737_kitchen%202%20(8).png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c017dcf4ca11d299a77859_image%2022%20(1).png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c0182d7e4ae2e81dde905d_kitchen%202%20(9).png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c01873c65b8862401f3d7c_repl.png",
+      ],
+    },
+    dark: {
+      mainImg:
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c018e3a4cbc79aa1a8c732_image%2021%20(2).png",
+      sliders: [
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c01923ab3fd8f47cd8a818_kitchen%202%20(10).png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c019658120db5049c2fbae_image%2022%20(2).png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c019a93684ac48c394b393_kitchen%202%20(11).png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c019e09c1a33194f61dd86_replic.png",
+      ],
+    },
+    green: {
+      mainImg:
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c01a2a0da81a6bf79eb5d4_image%2021%20(3).png",
+      sliders: [
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c01a8014cbc515b286f2a3_kitchen%202%20(12).png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c01aee5aadd22937fec497_image%2022%20(3).png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c01b3966010ad14f9a1fab_kitchen%202%20(13).png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c01b88ab3fd8f47cd9fe23_replicate.png",
+      ],
+    },
+  };
+
+var soho__size_36 = {
+  white: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b8fe5832c3df1850d22264_image%2021.png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c10682b888d8bf41459ed5_kitchen%202%20(35).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c106b704daf8fdc62e65ec_image%2022%20(8).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c10703a884445bf8524d61_kitchen%202%20(36).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c1076cccc4cb9c462aba26_opendraw.png",
+    ],
+  },
+  grey: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b908ab0ccc78ff17b00df3_image%2021%20(1).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c107b71f8f89231f782aab_kitchen%202%20(37).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c107f4f50cbd5e8f248a31_image%2022%20(9).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c1083c49105ae42613409f_kitchen%202%20(38).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c1087be9d19e38b491a046_gopendraw.png",
+    ],
+  },
+  dark: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b916fd24b59260631d71ef_image%2021%20(2).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c108df94d491289aebd654_kitchen%202%20(39).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c109c2ec6e719a3879866b_image%2022%20(10).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c10a23339107a993fd9544_kitchen%202%20(40).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c10b8cba04957cab5d4f9a_dopendraw.png",
+    ],
+  },
+  green: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b920971f81a746db393fc4_image%2021%20(3).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c10c23792107aa32fb5375_kitchen%202%20(41).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c10ca227d32908b0823868_image%2022%20(11).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c10da6ea46f8e32a843019_kitchen%202%20(42).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c10e854eadbe338462d700_gopendraw%20(1).png",
+    ],
+  },
+};
+
+var soho__size_30 = {
+  white: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b8fe5832c3df1850d22264_image%2021.png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c10682b888d8bf41459ed5_kitchen%202%20(35).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c106b704daf8fdc62e65ec_image%2022%20(8).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c10703a884445bf8524d61_kitchen%202%20(36).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c1076cccc4cb9c462aba26_opendraw.png",
+    ],
+  },
+  grey: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b908ab0ccc78ff17b00df3_image%2021%20(1).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c107b71f8f89231f782aab_kitchen%202%20(37).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c107f4f50cbd5e8f248a31_image%2022%20(9).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c1083c49105ae42613409f_kitchen%202%20(38).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c1087be9d19e38b491a046_gopendraw.png",
+    ],
+  },
+  dark: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b916fd24b59260631d71ef_image%2021%20(2).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c108df94d491289aebd654_kitchen%202%20(39).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c109c2ec6e719a3879866b_image%2022%20(10).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c10a23339107a993fd9544_kitchen%202%20(40).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c10b8cba04957cab5d4f9a_dopendraw.png",
+    ],
+  },
+  green: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b920971f81a746db393fc4_image%2021%20(3).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c10c23792107aa32fb5375_kitchen%202%20(41).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c10ca227d32908b0823868_image%2022%20(11).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c10da6ea46f8e32a843019_kitchen%202%20(42).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c10e854eadbe338462d700_gopendraw%20(1).png",
+    ],
+  },
+};
+
+
+// Get references to size and color buttons
+const sizeButtons03 = document.querySelectorAll("#soho .size-btn");
+const colorButtons03 = document.querySelectorAll("#soho .variant-btn");
+
+// Function to update images based on selected size and color
+function updateImages03(size, color) {
+  const sizeObject = window[`soho__size_${size}`];
+
+  const colorObject = sizeObject[color];
+
+  if (sizeObject && colorObject) {
+    // Set main image URL
+    var mainImageElement = document.querySelector("#soho .images-block img");
+    if (mainImageElement) {
+      mainImageElement.src = colorObject.mainImg;
+      mainImageElement.srcset = colorObject.mainImg;
+    }
+
+    // Loop through sliders and set new src attributes
+    var sliderImages = document.querySelectorAll("#soho .slide-item img ");
+
+    if (colorObject.sliders.length > 0) {
+      for (var i = 0; i < sliderImages.length; i++) {
+        var index = i % colorObject.sliders.length;
+        var sliderImage = colorObject.sliders[index];
+
+        if (sliderImages[i]) {
+          sliderImages[i].src = sliderImage;
+          sliderImages[i].srcset = sliderImage;
+        }
+      }
+    }
+  }
+}
+
+// Function to handle size button click
+function handleSizeButtonClick03(size) {
+  // Remove 'w--current' class from all size buttons
+  sizeButtons03.forEach((button) => button.classList.remove("size__current"));
+
+  // Add 'w--current' class to the clicked size button
+  this.classList.add("size__current");
+
+  // Reset color buttons (assuming 'white' is the default color)
+  colorButtons03.forEach((button) => button.classList.remove("active"));
+  colorButtons03[0].classList.add("active");
+
+  // Update images based on the selected size and default color
+  updateImages03(size, "white");
+}
+
+// Function to handle color button click
+function handleColorButtonClick03(color) {
+  // Remove 'active' class from all color buttons
+  colorButtons03.forEach((button) => button.classList.remove("active"));
+
+  // Add 'active' class to the clicked color button
+  this.classList.add("active");
+
+  // Get the selected size button with 'w--current' class
+  const selectedSizeButton = document.querySelector(
+    "#soho .size-btn.size__current"
+  );
+
+  // If a size button is selected, update images based on the selected size and color
+  if (selectedSizeButton) {
+    const selectedSize = selectedSizeButton.innerText.trim().split("”")[0]; //output 72"
+    updateImages03(selectedSize, color);
+  }
+}
+
+// Attach click event listeners to size buttons
+sizeButtons03.forEach((button) =>
+  button.addEventListener("click", function () {
+    handleSizeButtonClick03.call(this, this.innerText.trim().split("”")[0]);
+  })
+);
+
+// Attach click event listeners to color buttons
+colorButtons03.forEach((button) =>
+  button.addEventListener("click", function () {
+    handleColorButtonClick03.call(this, this.classList[2]); // Assuming the color class is the second class in the classList
+  })
+);
