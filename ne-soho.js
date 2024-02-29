8ab56e7c5cbb7c51f2db4348d7ad7df4770687fd
Dev <86075203+web-co1@users.noreply.github.com>
2024-03-01 02:24:14 +0500

    Create ne-soho.js

---
+++ ne-soho.js
@@ -0,0 +1,323 @@
+var soho__size_72 = {
+  white: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0fb7b206196c4fe4ee7c0_image%2022%20(1).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e4675360f327757011d1_Soho%2060%20w%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e4a49108f8f6f1d57f47_Soho%2060%20w%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e4bc4d45cf79359e57e5_Soho%2060%20w%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e4d01b2c6d736194bec7_Soho%2060%20w%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e4e9fb6adb26a68d4191_Soho%2060%20w%203_1%20(1).png",
+    ],
+  },
+  grey: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0fbeb17e8b72dc9febe6a_image%2021%20(1).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e61393df6cf15287f908_Soho%2060%20gray%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e630fae4dda08e65baf1_Soho%2060%20gray%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e647c5c6ac4a32414465_Soho%2060%20gray%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e6590eaccae0d23a5f2b_Soho%2060%20gray%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e674ec14259dc6d0762d_Soho%2060%20gray%203_1%20(1).png",
+    ],
+  },
+
+  dark: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0fc76afa8fa3df2e3a9e6_image%2021%20(2).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e725163320109327bac5_Soho%2060%20b%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e73ee3727ae5723f3a3f_Soho%2060%20b%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e752098f7a096a4f7932_Soho%2060%20b%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e7664d09c163b4cbe059_Soho%2060%20b%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e77c67babb74a4d68f94_Soho%2060%20b%203_1%20(1).png",
+    ],
+  },
+
+  green: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0fcc1661b879f3451bc8f_image%2021%20(3).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e8124d09c163b4cc4789_Soho%2060%20g%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e828f69c1ce7863617d6_Soho%2060%20g%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e848e7f22e8f5a22825d_Soho%2060%20g%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e860d12dabcd59ed9656_Soho%2060%20g%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e876907624378c82c924_Soho%2060%20g%203_1%20(1).png",
+    ],
+  },
+};
+
+var soho__size_60 = {
+  white: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0fb7b206196c4fe4ee7c0_image%2022%20(1).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e4675360f327757011d1_Soho%2060%20w%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e4a49108f8f6f1d57f47_Soho%2060%20w%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e4bc4d45cf79359e57e5_Soho%2060%20w%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e4d01b2c6d736194bec7_Soho%2060%20w%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e4e9fb6adb26a68d4191_Soho%2060%20w%203_1%20(1).png",
+    ],
+  },
+  grey: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0fbeb17e8b72dc9febe6a_image%2021%20(1).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e61393df6cf15287f908_Soho%2060%20gray%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e630fae4dda08e65baf1_Soho%2060%20gray%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e647c5c6ac4a32414465_Soho%2060%20gray%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e6590eaccae0d23a5f2b_Soho%2060%20gray%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e674ec14259dc6d0762d_Soho%2060%20gray%203_1%20(1).png",
+    ],
+  },
+
+  dark: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0fc76afa8fa3df2e3a9e6_image%2021%20(2).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e725163320109327bac5_Soho%2060%20b%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e73ee3727ae5723f3a3f_Soho%2060%20b%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e752098f7a096a4f7932_Soho%2060%20b%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e7664d09c163b4cbe059_Soho%2060%20b%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e77c67babb74a4d68f94_Soho%2060%20b%203_1%20(1).png",
+    ],
+  },
+
+  green: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b0fcc1661b879f3451bc8f_image%2021%20(3).jpg",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e8124d09c163b4cc4789_Soho%2060%20g%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e828f69c1ce7863617d6_Soho%2060%20g%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e848e7f22e8f5a22825d_Soho%2060%20g%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e860d12dabcd59ed9656_Soho%2060%20g%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0e876907624378c82c924_Soho%2060%20g%203_1%20(1).png",
+    ],
+  },
+};
+
+var soho__size_48 = {
+    white: {
+      mainImg:
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c015bf1b67fbfec935e85d_image%2021.png",
+      sliders: [
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0eaa36c3a9eff59b9d4c3_Soho%2048%20w%201_2%20(1).png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0eaca0b9b8a73d19dba09_Soho%2048%20w%202_2%20(1).png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0eadc577f04001fe4230a_Soho%2048%20w%202_1%20(1).png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0eafa098f7a096a515448_Soho%2048%20w%201_1%20(1).png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0eb0c3f8d1ed5c0dce626_Soho%2048%20w%203%20(1).png",
+      ],
+    },
+    grey: {
+      mainImg:
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c017494e8a00ec77b58761_image%2021%20(1).png",
+      sliders: [
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0ebc8adee38aacf7f27a9_Soho%2048%20gray%201_2%20(1).png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0ebec2b7d4da368b0dd62_Soho%2048%20gray%202_2%20(1).png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0ebfe3f8d1ed5c0dd4e4e_Soho%2048%20gray%202_1%20(1).png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0ec0f07456f1ee73c3f2c_Soho%2048%20gray%201_1%20(1).png",
+         "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0ec215c3ed0b0aec715c5_Soho%2048%20gray%203%20(1).png",
+      ],
+    },
+    dark: {
+      mainImg:
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65c018e3a4cbc79aa1a8c732_image%2021%20(2).png",
+      sliders: [
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0ecbd6f812a8af6b19fb9_Soho%2048%20b%201_2%20(1)%20(1).png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0ecce379a54f3d627b926_Soho%2048%20b%202_2%20(1).png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0ece3c0b7991d0b4b36d9_Soho%2048%20b%202_1%20(1).png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0ecf69a98a72494ced071_Soho%2048%20b%201_1%20(1).png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0ed0aa2552826e2178b75_Soho%2048%20b%203%20(1).png",
+      ],
+    },
+    green: {
+      mainImg:
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65d3befa87b73afc7998905b_Mask%20group%20(36).jpg",
+      sliders: [
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0ed7de8e652ea35f22e2e_Soho%2048%20g%201_2%20(1).png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0edff0af958cf135a35e9_Soho%2048%20g%202_2%20(1).png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0ee15b9249f5e5d6f09a9_Soho%2048%20g%202_1%20(1).png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0ee25340eae152663e88b_Soho%2048%20g%201_1%20(1).png",
+        "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0ee3717b9674abccec703_Soho%2048%20g%203%20(1).png",
+      ],
+    },
+  };
+
+var soho__size_36 = {
+  white: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b8fe5832c3df1850d22264_image%2021.png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f05c0af958cf135b73ab_Soho%2036%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f0786256fa9a9fcac688_Soho%2036%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f08b5b4467684b67c83d_Soho%2036%202_1%20(2).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f09d7da6cf65fe4a2106_Soho%2036%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f0aef0e771f9e518ef52_Soho36%20w%203_1%20(1).png",
+    ],
+  },
+  grey: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b908ab0ccc78ff17b00df3_image%2021%20(1).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f166945190b4e53b794d_Soho36%20grey%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f186f1c2ef08c77ccd85_Soho36%20grey%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f1aafe9934ae5bb149fa_Soho36%20grey%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f1d070371d863c6bb426_Soho36%20grey%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f1e3f553eb9b723c6b0a_Soho36%20grey%203_1%20(1).png",
+    ],
+  },
+  dark: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b916fd24b59260631d71ef_image%2021%20(2).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f2dcd72df0e21c05aff0_Soho36%20b%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f2fa7f6a7b088ade3e4d_Soho36%20b%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f30dda172ea71bb3ca87_Soho36%20b%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f32885dba77084e1c22f_Soho36%20b%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f33c7116e0780e5a0bf9_Soho36%20b%203_1%20(1).png",
+    ],
+  },
+  green: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b920971f81a746db393fc4_image%2021%20(3).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f3c2de7800a0c712dc16_Soho36%20g%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f3cd659a83dc5194556d_Soho36%20g%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f3dfb260ac7989c68b1c_Soho36%20g%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f3f114e9235e3db70d24_Soho36%20g%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f405d01ca86c67420ae1_Soho36%20g%203_1%20(1).png",
+    ],
+  },
+};
+
+var soho__size_30 = {
+  white: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b8fe5832c3df1850d22264_image%2021.png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f05c0af958cf135b73ab_Soho%2036%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f0786256fa9a9fcac688_Soho%2036%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f08b5b4467684b67c83d_Soho%2036%202_1%20(2).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f09d7da6cf65fe4a2106_Soho%2036%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f0aef0e771f9e518ef52_Soho36%20w%203_1%20(1).png",
+    ],
+  },
+  grey: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b908ab0ccc78ff17b00df3_image%2021%20(1).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f166945190b4e53b794d_Soho36%20grey%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f186f1c2ef08c77ccd85_Soho36%20grey%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f1aafe9934ae5bb149fa_Soho36%20grey%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f1d070371d863c6bb426_Soho36%20grey%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f1e3f553eb9b723c6b0a_Soho36%20grey%203_1%20(1).png",
+    ],
+  },
+  dark: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b916fd24b59260631d71ef_image%2021%20(2).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f2dcd72df0e21c05aff0_Soho36%20b%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f2fa7f6a7b088ade3e4d_Soho36%20b%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f30dda172ea71bb3ca87_Soho36%20b%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f32885dba77084e1c22f_Soho36%20b%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f33c7116e0780e5a0bf9_Soho36%20b%203_1%20(1).png",
+    ],
+  },
+  green: {
+    mainImg:
+      "https://assets-global.website-files.com/658341e50aaaf9480b039557/65b920971f81a746db393fc4_image%2021%20(3).png",
+    sliders: [
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f3c2de7800a0c712dc16_Soho36%20g%201_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f3cd659a83dc5194556d_Soho36%20g%202_2%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f3dfb260ac7989c68b1c_Soho36%20g%202_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f3f114e9235e3db70d24_Soho36%20g%201_1%20(1).png",
+      "https://uploads-ssl.webflow.com/658341e50aaaf9480b039557/65e0f405d01ca86c67420ae1_Soho36%20g%203_1%20(1).png",
+    ],
+  },
+};
+
+
+// Get references to size and color buttons
+const sizeButtons03 = document.querySelectorAll("#soho .size-btn");
+const colorButtons03 = document.querySelectorAll("#soho .variant-btn");
+
+// Function to update images based on selected size and color
+function updateImages03(size, color) {
+  const sizeObject = window[`soho__size_${size}`];
+
+  const colorObject = sizeObject[color];
+
+  if (sizeObject && colorObject) {
+    // Set main image URL
+    var mainImageElement = document.querySelector("#soho .images-block img");
+    if (mainImageElement) {
+      mainImageElement.src = colorObject.mainImg;
+      mainImageElement.srcset = colorObject.mainImg;
+    }
+
+    // Loop through sliders and set new src attributes
+    var sliderImages = document.querySelectorAll("#soho .slide-item img ");
+
+    if (colorObject.sliders.length > 0) {
+      for (var i = 0; i < sliderImages.length; i++) {
+        var index = i % colorObject.sliders.length;
+        var sliderImage = colorObject.sliders[index];
+
+        if (sliderImages[i]) {
+          sliderImages[i].src = sliderImage;
+          sliderImages[i].srcset = sliderImage;
+        }
+      }
+    }
+  }
+}
+
+// Function to handle size button click
+function handleSizeButtonClick03(size) {
+  // Remove 'w--current' class from all size buttons
+  sizeButtons03.forEach((button) => button.classList.remove("size__current"));
+
+  // Add 'w--current' class to the clicked size button
+  this.classList.add("size__current");
+  
+  const Color = document.querySelector("#soho .variant-btn.active")
+    .classList[2];
+
+  // Update images based on the selected size and default color
+  updateImages03(size, Color);
+}
+
+// Function to handle color button click
+function handleColorButtonClick03(color) {
+  // Remove 'active' class from all color buttons
+  colorButtons03.forEach((button) => button.classList.remove("active"));
+
+  // Add 'active' class to the clicked color button
+  this.classList.add("active");
+
+  // Get the selected size button with 'w--current' class
+  const selectedSizeButton = document.querySelector(
+    "#soho .size-btn.size__current"
+  );
+
+  // If a size button is selected, update images based on the selected size and color
+  if (selectedSizeButton) {
+    const selectedSize = selectedSizeButton.innerText.trim().split("”")[0]; //output 72"
+    updateImages03(selectedSize, color);
+  }
+}
+
+// Attach click event listeners to size buttons
+sizeButtons03.forEach((button) =>
+  button.addEventListener("click", function () {
+    handleSizeButtonClick03.call(this, this.innerText.trim().split("”")[0]);
+  })
+);
+
+// Attach click event listeners to color buttons
+colorButtons03.forEach((button) =>
+  button.addEventListener("click", function () {
+    handleColorButtonClick03.call(this, this.classList[2]); // Assuming the color class is the second class in the classList
+  })
+);
